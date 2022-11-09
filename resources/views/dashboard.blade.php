88aae20e7ca64c04ff4f7db646913e1e82b66756
I Gede Deindra Dwija Puridiasta <114994571+DeindraDwija@users.noreply.github.com>
2022-11-09 13:07:02 +0800

    Add files via upload

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,28 @@
+<x-app-layout>
+<main>
+        <div>          
+            <div class="flex items-center justify-center content-center p-3">
+                <div class="space-y-5 py-5">
+                    <div class="flex gap-3 flex-row max-md:flex-col">
+                        <div class="max-w-sm rounded-md overflow-hidden shadow-2xl hover:scale-105 transition-transform flex">
+                            <a href="">
+                            <img class="min-w-0 justify-center pr-20" src="https://i.pinimg.com/564x/8b/a6/ae/8ba6aea4f2886accca05b46995646b45.jpg">
+                            <div class="px-6 py-4">
+                            <div class="font-bold text-xl mb-2 text-black text-center">Kecak Free Dance</div>
+                            </div>
+                            </a>
+                        </div>
+                        <div class="max-w-sm rounded-md overflow-hidden shadow-2xl hover:scale-105 transition-transform ">
+                            <a href="">
+                            <img class="min-w-0 justify-center ml-20 box-decoration-clone bg-white " src="https://i.pinimg.com/564x/8b/a6/ae/8ba6aea4f2886accca05b46995646b45.jpg">
+                            <div class="px-6 py-4">
+                            <div class="font-bold text-xl mb-2 text-black text-center">Kecak Free Dance With Family</div>
+                            </div>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
+</x-app-layout>
